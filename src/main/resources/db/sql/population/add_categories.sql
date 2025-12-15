1e1f5e66d361ac45f747cd8ce0dd02573d3fc05e
GritsH <grits.lena@gmail.com>
2025-12-15 10:26:41 +0300

    populate category and frequency tables

---
+++ src/main/resources/db/sql/population/add_categories.sql
@@ -0,0 +1,10 @@
+insert into habittracker.habit_category (category_name)
+values ('PHYSICAL_HEALTH'),
+       ('MENTAL_HEALTH'),
+       ('RELATIONSHIP'),
+       ('PROFESSIONAL_DEVELOPMENT'),
+       ('FINANCIAL'),
+       ('ARTS_AND_CRAFTS'),
+       ('ENVIRONMENTAL'),
+       ('HOME'),
+       ('OTHER');
