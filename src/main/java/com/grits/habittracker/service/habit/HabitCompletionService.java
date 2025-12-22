a581687e78179ce7af337e89f216314907320ab4
GritsH <grits.lena@gmail.com>
2025-12-22 13:20:41 +0300

    add transactional annotation

--- src/main/java/com/grits/habittracker/service/habit/HabitCompletionService.java
+++ src/main/java/com/grits/habittracker/service/habit/HabitCompletionService.java
@@ -7,12 +7,14 @@ import com.grits.habittracker.model.response.HabitCompletionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class HabitCompletionService {
 
     private final HabitCompletionDao completionDao;
