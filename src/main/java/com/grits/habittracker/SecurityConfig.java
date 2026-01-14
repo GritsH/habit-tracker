2ded6fda5cab34b14085d02eeb70c7bae7908fc9
GritsH <61628266+GritsH@users.noreply.github.com>
2026-01-14 11:17:26 +0300

    add health check

--- src/main/java/com/grits/habittracker/SecurityConfig.java
+++ src/main/java/com/grits/habittracker/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/api-docs/**",
+                                "/actuator/**",
                                 "/v1/**"
                         ).permitAll()
                         .anyRequest().authenticated()
